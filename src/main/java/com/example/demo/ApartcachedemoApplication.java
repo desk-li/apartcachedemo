cdc8b14c249ed74ab5af5d8e3c74de18329ec46d
desk-li <306843840@qq.com>
2021-08-25 14:46:10 +0800

    有缓存功能的service接口数据入库mysql

--- src/main/java/com/example/demo/ApartcachedemoApplication.java
+++ src/main/java/com/example/demo/ApartcachedemoApplication.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import com.apartcache.starter.config.interceptor.CacheAdvice;
 import com.apartcache.starter.config.redis.RedisCacheManager;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(value = "com.apartcache.starter.mapper")
 @SpringBootApplication
 public class ApartcachedemoApplication {
 
--- src/test/java/com/example/demo/ApartcachedemoApplicationTests.java
+++ src/test/java/com/example/demo/ApartcachedemoApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.demo;
 
+import com.apartcache.starter.config.CacheNameGenerator;
+import com.apartcache.starter.config.ServiceManager;
+import com.apartcache.starter.config.interceptor.CacheAdvice;
+import com.apartcache.starter.config.redis.RedisCacheManager;
+import com.apartcache.starter.mapper.CacheDataMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ApartcachedemoApplicationTests {
 
+    @Autowired
+    ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        CacheDataMapper bean = applicationContext.getBean(CacheDataMapper.class);
+        String[] beanNamesForType = applicationContext.getBeanNamesForType(ServiceManager.class);
+        String[] beanNamesForType3 = applicationContext.getBeanNamesForType(RedisCacheManager.class);
+        String[] beanNamesForType2 = applicationContext.getBeanNamesForType(CacheAdvice.class);
+        String[] beanNamesForType1 = applicationContext.getBeanNamesForType(CacheNameGenerator.class);
+
+        Object cacheServiceManager = applicationContext.getBean("cacheServiceManager");
+        Object dBServiceManager = applicationContext.getBean("dBServiceManager");
+        Object cacheI = applicationContext.getBean("cacheI");
+        Integer n = bean.count();
+        System.out.println(bean.toString());
     }
 
 }
